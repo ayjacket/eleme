b8c3b3839b2e03a0d78b029b302d6a558c973abe
Yangjie <ayjacket@icloud.com>
2017-02-20 17:52:58 +0800

    first commit

---
+++ pages/goods/pay/pay.js
@@ -0,0 +1,22 @@
+Page({
+    data:{
+        resultType:"",
+        resultContent:""
+    },
+    onLoad:function(options){
+        var resultType=options.resultType;
+        if(resultType=="success"){
+            this.setData({
+                resultType:"success",
+                resultContent:"支付成功",
+                url:'../../order/list/list?status=tosend'
+            });
+        }else{
+            this.setData({
+                resultType:"warn",
+                resultContent:"支付失败",
+                url:'../../order/list/list'
+            });
+        }
+    }
+});
